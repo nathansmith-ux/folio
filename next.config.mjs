8c283fb6a1c2a4b2e2cd8240ac561a3ee9e1427b
Nathan <nathanrmsmith@gmail.com>
2024-04-02 13:06:33 -0400

    Fixed remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['oaidalleapiprodscus.blob.core.windows.net'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'oaidalleapiprodscus.blob.core.windows.net' },
+    ],
   },
 };
 
