2f47b7dadada0aa5f301077c88c6af2ecfd4ab22
Nathan <nathanrmsmith@gmail.com>
2024-04-04 13:07:29 -0400

    Fixed bug in layout by adding global css

--- app/(marketing-site)/seo/page.tsx
+++ app/(marketing-site)/seo/page.tsx
@@ -4,15 +4,12 @@ import CardIconGrid from "@/components/ui/card/CardIconGrid"
 import CtaImage from "@/components/ui/cta/CtaImage"
 import CtaText from "@/components/ui/cta/CtaText"
 import { TypeWriterCta } from "@/components/ui/cta/TypeWriterCta"
-import Footer from "@/components/ui/footer/Footer"
-import NavBar from "@/components/ui/navigation/NavBar"
 import HeroWBullets from "@/components/ui/hero/HeroWBullets"
 import { words, services, process, bulletPoints } from "@/site-copy/seoPage"
 
 export default function SeoPage() {
   return (
     <main>
-      <NavBar />
       <AuroraBackground>
         <TypeWriterCta
           subtext="Start Your Organic Journey Today" 
@@ -44,7 +41,6 @@ export default function SeoPage() {
         header="Are You Ready To Take Your Business To The Digital World?"
         description="Let's carve out a piece of the internet for your business"
       />
-      <Footer />
     </main>
   )
 }
--- app/(marketing-site)/web-dev/page.tsx
+++ app/(marketing-site)/web-dev/page.tsx
@@ -4,15 +4,12 @@ import { TypeWriterCta } from "@/components/ui/cta/TypeWriterCta";
 import CtaImage from "@/components/ui/cta/CtaImage";
 import CardIconGrid from "@/components/ui/card/CardIconGrid";
 import HeroWBullets from "@/components/ui/hero/HeroWBullets";
-import NavBar from "@/components/ui/navigation/NavBar";
-import Footer from "@/components/ui/footer/Footer";
 import { AuroraBackground } from "@/components/ui/background/AuraBackground";
 import { bulletPoints, words, services, process } from "@/site-copy/webDevPage";
 
 export default function WebDevPage() {  
   return (
     <main>
-      <NavBar />
       <TextCentered 
         header="You've Established Your Vision. We'll Develop It to Scale"
         description="We develop end-to-end solutions for small business owners to help you carve out your piece of the digital world"
@@ -44,7 +41,6 @@ export default function WebDevPage() {
           cards={false}
         />
       </AuroraBackground>
-      <Footer />
     </main>
   )
 }
