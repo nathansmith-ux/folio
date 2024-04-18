bb05570ac96aa1aa437eaa38c5507034f1ca7f2b
Nathan <nathanrmsmith@gmail.com>
2024-04-18 15:44:55 -0400

    Add homepage actions using new format

--- app/api/home/action.tsx
+++ app/api/home/action.tsx
@@ -26,6 +26,36 @@ import {
   process as seoProcess
 } from "@/site-copy/seoPage";
 
+const generativeUi = [
+  {
+    header: "Cyber Security Gen UI",
+    description: "Get realtime answers to cyber security questions and analyze urls",
+    image: "/cyber-security/cyber-ai.webp",
+    link: "/generative-ui/cyber-security"
+  },
+  {
+    header: "Life Science Gen UI",
+    description: "Find scientific journals and learn more about different diseases",
+    image: "/life-science/life-science-ai.webp",
+    link: "/generative-ui/life-sciences"
+  }
+]
+
+const projects = [
+  {
+    header: "TaiL",
+    description: "An AI platform built using Nextjs 14 to create immersive choose your own adventure games",
+    image: "/tail-assets/what-is-tail.webp",
+    link: "/projects/tail"
+  },
+  {
+    header: "Schema Forge",
+    description: "An SEO platform built designed to make the generation of schema markup more efficent",
+    image: "/schema-forge-assets/coloredLogo.png",
+    link: "/projects/schema-forge"
+  }
+]
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
@@ -85,15 +115,46 @@ async function submitUserMessage(userInput: string) {
         name: 'service_information',
         description: 'Provide information about web development and seo services',
         parameters: z.object({}).required(),
+      },
+      {
+        name: 'web_dev_information',
+        description: 'Provide information about web development services',
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'seo_information',
+        description: 'Provide information about SEO services',
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'seo_process',
+        description: 'Provide information about our SEO process',
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'web_dev_process',
+        description: 'Provide information about our web development process',
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'generative_ui',
+        description: 'Provide information about what generative ui is',
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'project_information',
+        description: `Provide information about Nathan's projects including TaiL and Schema Forge`,
+        parameters: z.object({}).required(),
+      },
+      {
+        name: 'contact_us',
+        description: 'Provide contact information and ways to contact White Walls Media',
+        parameters: z.object({}).required(),
       }
     ]
   });
 
     completion.onFunctionCall('service_information', async () => {
-      reply.update(
-        <div>Loading...</div>
-      )
-
       reply.done(
         <div>
           <CtaText 
@@ -109,182 +170,150 @@ async function submitUserMessage(userInput: string) {
           role: 'function',
           name: 'service_information',
           content: `Explore our different offerings by simplying asking what are your seo (or web development) services`,
-        },
+        }
       ])
     })
-//     functions: {
-//       service_information: {
-//         description: 'Provide information about web development and seo services',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
-          
-//           yield <Spinner />
 
-//           return (
-//             <div>
-//               <CtaText 
-//               header="We Offer Both Web Development & SEO Services"
-//               description="Explore our different offerings by simplying asking what are your seo (or web development) services"
-//               />
-//             </div>
-//           );
-//         }
-//       },
-//       web_dev_information: {
-//         description: 'Provide information about web development services',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
-          
-//           yield <Spinner />
+    completion.onFunctionCall('web_dev_information', async () => {
+      reply.done(
+        <div>
+          <CardGrid 
+            header="We Provide The Following Web Development Services"
+            services={webServices}
+          />
+        </div>
+      )
 
-//           return (
-//             <div>
-//               <CardGrid 
-//                 header="We Provide The Following Web Development Services"
-//                 services={webServices}
-//               />
-//             </div>
-//           );
-//         }
-//       },
-//       seo_information: {
-//         description: 'Provide information about SEO services',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
-          
-//           yield <Spinner />
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'web_dev_information',
+          content: `${webServices}`,
+        }
+      ])
+    })
 
-//           return (
-//             <div>
-//               <CardGrid 
-//                 header="We Provide The Following SEO Services"
-//                 services={seoServices}
-//               />
-//             </div>
-//           );
-//         }
-//       },
-//       seo_process: {
-//         description: 'Provide information about our SEO process',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
-          
-//           yield <Spinner />
+    completion.onFunctionCall('seo_information', async () => {
+      reply.done(
+        <div>
+          <CardGrid 
+            header="We Provide The Following SEO Services"
+            services={seoServices}
+          />
+        </div>
+      )
 
-//           return (
-//             <div>
-//               <CardIconGrid 
-//                 header="We Have A Four Step SEO process"
-//                 process={seoProcess}  
-//               />
-//             </div>
-//           );
-//         }
-//       },
-//       web_dev_process: {
-//         description: 'Provide information about our web development process',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
-          
-//           yield <Spinner />
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'seo_information',
+          content: `${seoServices}`,
+        }
+      ])
+    })
 
-//           return (
-//             <div>
-//               <CardIconGrid 
-//                 header="We Have A Four Step Web Development process"
-//                 process={webProcess}
-//               />
-//             </div>
-//           );
-//         }
-//       },
-//       generative_ui : {
-//         description: 'Provide information about what generative ui is',
-//         parameters: z.object({}).required(),
-//         render: async function*() {
+    completion.onFunctionCall('seo_process', async () => {
+      reply.done(
+        <div>
+          <CardIconGrid 
+            header="We Have A Four Step SEO process"
+            process={seoProcess}  
+          />
+        </div>
+      )
 
-//           yield <Spinner />
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'seo_process',
+          content: `${seoProcess}`,
+        }
+      ])
+    })
 
-//           const generativeUi = [
-//             {
-//               header: "Cyber Security Gen UI",
-//               description: "Get realtime answers to cyber security questions and analyze urls",
-//               image: "/cyber-security/cyber-ai.webp",
-//               link: "/generative-ui/cyber-security"
-//             },
-//             {
-//               header: "Life Science Gen UI",
-//               description: "Find scientific journals and learn more about different diseases",
-//               image: "/life-science/life-science-ai.webp",
-//               link: "/generative-ui/life-sciences"
-//             }
-//           ]
+    completion.onFunctionCall('web_dev_process', async () => {
+      reply.done(
+        <div>
+          <CardIconGrid 
+            header="We Have A Four Step Web Development Process"
+            process={webProcess}  
+          />
+        </div>
+      )
 
-//           return (
-//             <div>
-//               <ImageCardGrid 
-//                 header="Our Different Gen UI Experiences"
-//                 cardContent={generativeUi}
-//               />
-//             </div>
-//           )
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'web_dev_process',
+          content: `${webProcess}`,
+        }
+      ])
+    })
 
-//         }
-//       },
-//       project_information: {
-//         description: `Provide information about Nathan's projects including TaiL and Schema Forge`,
-//         parameters: z.object({}).required(),
-//         render: async function*() {
+    completion.onFunctionCall('generative_ui', async () => {
+      reply.done(
+        <div>
+          <ImageCardGrid 
+            header="Our Different Gen UI Experiences"
+            cardContent={generativeUi}
+          />
+        </div>
+      )
 
-//           yield <Spinner />
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'generative_ui',
+          content: `${generativeUi}`,
+        }
+      ])
+    })
 
-//           const projects = [
-//             {
-//               header: "TaiL",
-//               description: "An AI platform built using Nextjs 14 to create immersive choose your own adventure games",
-//               image: "/tail-assets/what-is-tail.webp",
-//               link: "/projects/tail"
-//             },
-//             {
-//               header: "Schema Forge",
-//               description: "An SEO platform built designed to make the generation of schema markup more efficent",
-//               image: "/schema-forge-assets/coloredLogo.png",
-//               link: "/projects/schema-forge"
-//             }
-//           ]
+    completion.onFunctionCall('project_information', async () => {
+      reply.done(
+        <div>
+          <ImageCardGrid 
+            header="My Different Projects"
+            cardContent={projects}
+          />
+        </div>
+      )
 
-//           return (
-//             <div>
-//               <ImageCardGrid 
-//                 header="My Different Projects"
-//                 cardContent={projects}
-//               />
-//             </div>
-//           )
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'project_information',
+          content: `${projects}`,
+        }
+      ])
+    })
 
-//         }
-//       },
-//       contact_us: {
-//         description: `Provide contact information and ways to contact White Walls Media`,
-//         parameters: z.object({}).required(),
-//         render: async function*() {
+    completion.onFunctionCall('contact_us', async () => {
+      reply.done(
+        <div>
+          <CtaText 
+            header="Are You Ready To Take Your Business To The Digital World?"
+            description="Let's carve out a piece of the internet for your business"
+          />
+        </div>
+      )
 
-//           yield <Spinner />
-
-//           return (
-//             <div>
-//               <CtaText 
-//                 header="Are You Ready To Take Your Business To The Digital World?"
-//                 description="Let's carve out a piece of the internet for your business"
-//               />
-//             </div>
-//           )
-
-//         }
-//       }
-//     }
-//   })
-
+      aiState.done([
+        ...aiState.get(),
+        {
+          role: 'function',
+          name: 'contact_us',
+          content: `Are You Ready To Take Your Business To The Digital World? Let's carve out a piece of the internet for your business`,
+        }
+      ])
+    })
+    
   return {
     id: Date.now(),
     display: reply.value
