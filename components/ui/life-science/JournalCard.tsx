273130ce7c54d8b6f4db0b0053d270daef334fa0
Nathan <nathanrmsmith@gmail.com>
2024-04-17 23:57:58 -0400

    Fixed html errors

--- components/ui/life-science/JournalCard.tsx
+++ components/ui/life-science/JournalCard.tsx
@@ -44,7 +44,7 @@ export default function JournalCard({ header, description, link, publicationName
       >
           View The Paper
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
           </svg>
       </Link>
     </div>
--- components/ui/navigation/MobileNavBar.tsx
+++ components/ui/navigation/MobileNavBar.tsx
@@ -36,7 +36,7 @@ export default function MobileNavBar() {
           >
             <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
               </svg>
           </button>
           {isMenuOpen && (
